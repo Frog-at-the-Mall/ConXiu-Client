4aceba30243b235428ed2c88c37b00e67e60dc80
Bunndy <proutybrendan@gmail.com>
2022-02-27 19:19:46 -0500

    Added storing JWT under shared prefs

--- app/src/main/java/com/example/nfcproject/LoginActivity.java
+++ app/src/main/java/com/example/nfcproject/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.nfcproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -53,6 +54,11 @@ public class LoginActivity extends Activity {
     // Volley Request queue
     RequestQueue mQueue;
 
+
+    //***   Shared Prefs    ***//
+
+    public static final String SHARED_PREF = "sharedPref";
+    public static final String JWT = "jwt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -132,6 +138,7 @@ public class LoginActivity extends Activity {
         startActivity(intent);
     }
 
+
     // Login if user entered correct username and password.
     public void login() {
         username_str = editT_username.getText().toString().trim();
@@ -155,6 +162,13 @@ public class LoginActivity extends Activity {
                     e.printStackTrace();
                 }
                 if (successCheck == true) {
+                    SharedPreferences prefs = getSharedPreferences(SHARED_PREF, MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    try {
+                        editor.putString(JWT, (String) response.get("token"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                     Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
                     openMainPage(username_str);
                 } else {
