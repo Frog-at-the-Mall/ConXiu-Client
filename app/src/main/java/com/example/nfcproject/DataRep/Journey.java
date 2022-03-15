50a9d2a195573bf68d69e5588d126108f6a2ec41
E-Ber <emb09101@vtc.vsc.edu>
2022-03-15 15:21:52 -0400

    Oops. Journey has been added.

--- app/src/main/java/com/example/nfcproject/DataRep/Journey.java
+++ app/src/main/java/com/example/nfcproject/DataRep/Journey.java
@@ -1,9 +1,59 @@
 package com.example.nfcproject.DataRep;
 
-public class Journey {
-	String currentMeditation;
-	double[] coords;
-	byte[] capsule;
-	final String journeyName = "blah";
-	final int journeyID = -1000;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+public class Journey implements Comparable{
+
+	private String journeyName;
+	private int journeyID;
+	private Capsule capsule;
+
+	private String currentMeditation;
+	private double latitude;
+	private double longitude;
+
+	public Journey(String jName, int jId, Capsule caps){
+		this.capsule = caps;
+		this.journeyName = jName;
+		this.journeyID = jId;
+		this.currentMeditation = this.capsule.getMessage().toString();
+		this.latitude = this.capsule.getCoords()[0];
+		this.longitude = this.capsule.getCoords()[1];
+	}
+
+	public Journey nextShrine(SecretKey key) throws InvalidAlgorithmParameterException, IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+		return new Journey(getJourneyName(), getJourneyID(), capsule.innerCapsule(key));
+	}
+
+	public int getJourneyID(){
+		return this.journeyID;
+	}
+
+	public String getJourneyName(){
+		return this.journeyName;
+	}
+	public String getCurrentMeditation(){
+		return this.currentMeditation;
+	}
+	public double getShrineLatitude(){
+		return this.latitude;
+	}
+	public double getGetShrineLongitude(){
+		return this.longitude;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if (this.getClass() != o.getClass()) throw new IllegalArgumentException();
+		if (o == null) throw new NullPointerException();
+		Journey j = (Journey) o;
+		return this.getJourneyID()-j.getJourneyID();
+	}
 }
